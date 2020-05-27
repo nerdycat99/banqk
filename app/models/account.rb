8d9eeb43382fbaaafd54345166d8818277f813ae
nerdycat99 <info@drysky.co.uk>
2020-05-27 14:54:45 +0000

    added transactions model and capability to reflect transactions in the displayed balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,7 @@
 class Account < ApplicationRecord
   belongs_to :user
 
+  has_many :sent_transactions, :class_name => 'Transaction', :foreign_key => 'payer_id'
+  has_many :received_transactions, :class_name => 'Transaction', :foreign_key => 'payee_id'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_053952) do
+ActiveRecord::Schema.define(version: 2020_05_27_091259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2020_05_27_053952) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.decimal "amount"
+    t.bigint "payee_id"
+    t.bigint "payer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["payee_id"], name: "index_transactions_on_payee_id"
+    t.index ["payer_id"], name: "index_transactions_on_payer_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +46,6 @@ ActiveRecord::Schema.define(version: 2020_05_27_053952) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "transactions", "accounts", column: "payee_id"
+  add_foreign_key "transactions", "accounts", column: "payer_id"
 end
